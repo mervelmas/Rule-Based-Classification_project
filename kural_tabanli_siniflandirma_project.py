792cc945e5d77969acbb0420a8216eb76bcc7535
mervelmas <115989897+mervelmas@users.noreply.github.com>
2022-10-17 14:31:21 +0300

    Update kural_tabanli_siniflandirma_project.py

--- kural_tabanli_siniflandirma_project.py
+++ kural_tabanli_siniflandirma_project.py
@@ -1,50 +1,4 @@
 #############################################
-# Kural Tabanlı Sınıflandırma ile Potansiyel Müşteri Getirisi Hesaplama
-#############################################
-
-#############################################
-# İş Problemi
-#############################################
-# Bir oyun şirketi müşterilerinin bazı özelliklerini kullanarak seviye tabanlı (level based) yeni müşteri tanımları (persona)
-# oluşturmak ve bu yeni müşteri tanımlarına göre segmentler oluşturup bu segmentlere göre yeni gelebilecek müşterilerin şirkete
-# ortalama ne kadar kazandırabileceğini tahmin etmek istemektedir.
-
-# Örneğin: Türkiye’den IOS kullanıcısı olan 25 yaşındaki bir erkek kullanıcının ortalama ne kadar kazandırabileceği belirlenmek isteniyor.
-
-
-#############################################
-# Veri Seti Hikayesi
-#############################################
-# Persona.csv veri seti uluslararası bir oyun şirketinin sattığı ürünlerin fiyatlarını ve bu ürünleri satın alan kullanıcıların bazı
-# demografik bilgilerini barındırmaktadır. Veri seti her satış işleminde oluşan kayıtlardan meydana gelmektedir. Bunun anlamı tablo
-# tekilleştirilmemiştir. Diğer bir ifade ile belirli demografik özelliklere sahip bir kullanıcı birden fazla alışveriş yapmış olabilir.
-
-# Price: Müşterinin harcama tutarı
-# Source: Müşterinin bağlandığı cihaz türü
-# Sex: Müşterinin cinsiyeti
-# Country: Müşterinin ülkesi
-# Age: Müşterinin yaşı
-
-################# Uygulama Öncesi #####################
-
-#    PRICE   SOURCE   SEX COUNTRY  AGE
-# 0     39  android  male     bra   17
-# 1     39  android  male     bra   17
-# 2     49  android  male     bra   17
-# 3     29  android  male     tur   17
-# 4     49  android  male     tur   17
-
-################# Uygulama Sonrası #####################
-
-#       customers_level_based        PRICE SEGMENT
-# 0   BRA_ANDROID_FEMALE_0_18  1139.800000       A
-# 1  BRA_ANDROID_FEMALE_19_23  1070.600000       A
-# 2  BRA_ANDROID_FEMALE_24_30   508.142857       A
-# 3  BRA_ANDROID_FEMALE_31_40   233.166667       C
-# 4  BRA_ANDROID_FEMALE_41_66   236.666667       C
-
-
-#############################################
 # PROJE GÖREVLERİ
 #############################################
 
@@ -56,7 +10,7 @@
 
 import pandas as pd
 pd.set_option('display.max_columns', None)
-df = pd.read_csv("C:\\Users\\Merve.Taner\\Desktop\\datasets\\persona.csv")
+df = pd.read_csv("C:\Users\\Merve.Elmas\Desktop\datasets\persona.csv")
 df.head()
 
 df.info()
@@ -147,9 +101,6 @@ agg_df.head()
 # Age sayısal değişkenini kategorik değişkene çeviriniz.
 # Aralıkları ikna edici olacağını düşündüğünüz şekilde oluşturunuz.
 # Örneğin: '0_18', '19_23', '24_30', '31_40', '41_70'
-
-agg_df["AGE"].max() #66
-agg_df["AGE"].min() #15
 
 agg_df["AGE_CAT"] = pd.cut(agg_df["AGE"], [0, 18, 23, 30, 40, 70], labels=["0_18", "19_23", "24_30", "31_40", "41_70"])
 agg_df.head()
